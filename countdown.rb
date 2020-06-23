d2282d750194a3b68ffed66ec62eb9ac04f6be18
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-23 02:14:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,6 @@ seconds_to_midnight = 11
   seconds_to_midnight -= 1 
   
    puts "#{seconds_to_midnight} SECOND(S)!"
-   return "#{seconds_to_midnight} SECOND(S)!"
       sleep(5)
    
   end 
@@ -23,7 +22,6 @@ def countdown(seconds_to_midnight)
  while seconds_to_midnight > 0 
   seconds_to_midnight -= 1 
    puts "#{seconds_to_midnight} SECOND(S)!"
-   return "#{seconds_to_midnight} SECOND(S)!"
    
   end 
     return  "HAPPY NEW YEAR!"
