16748bb89fb442c08a8477b564dcc73e383f8f5b
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-23 01:56:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,30 @@
-#write your code here
+#require'pry'
 
-def countdown
+seconds_to_midnight = 11
+
+ def countdown_with_sleep(seconds_to_midnight)
+   
+   while seconds_to_midnight > 0 
+  seconds_to_midnight -= 1 
+  
+   puts "#{seconds_to_midnight} SECOND(S)!"
+      sleep(5)
+   
+  end 
+  
+    return  "HAPPY NEW YEAR!"
+
+end 
+
+
+def countdown(seconds_to_midnight)
+  
+ while seconds_to_midnight > 0 
+  seconds_to_midnight -= 1 
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   
+  end 
+    return  "HAPPY NEW YEAR!"
+    
 end
+
